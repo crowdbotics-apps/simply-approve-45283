dc3735d43307d129f1858bd92c80dd70e7fd4d46
Crowdbotics <team@crowdbotics.com>
2024-01-04 21:55:27 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "simply-approve-45283",
+  url: "https://simply-approve-45283.botics.co",
+  api: "https://simply-approve-45283.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
